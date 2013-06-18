0ba3038e0c34bae297b86fa06788aee4da98f615
DanSTurner <turner@nowwithmoredan.com>
2013-06-17 21:08:32 -0700

    I should probably be writing my own tests

--- bob2.rb
+++ bob2.rb
@@ -1,36 +1,34 @@
 class Bob
-
-
-  def self.response_for(input)
-    if self.shouting?(input)
-      puts 'Woah, chill out!'
-    elsif self.question?(input)
-      puts 'Sure.'
-    elsif self.statement?(input)
-      puts 'Whatever.'
+  def response_for(input)
+    if shouting?(input)
+      'Woah, chill out!'
+    elsif question?(input)
+      'Sure.'
+    elsif statement?(input)
+      'Whatever.'
     else
-      puts 'Fine, be that way.'
+      'Fine, be that way.'
     end
   end
 
-  def self.shouting?(input)
+  def shouting?(input)
   	!input.empty? && input == input.upcase
   end
 
-  def self.question?(input)
+  def question?(input)
   	!input.empty? && input[-1, 1] == "?"
   end
 
-  def self.statement?(input)
+  def statement?(input)
   	!input.empty?
   end
+
 #  loop do
 #    input = gets.chomp
-#    self.response_for(input)
-#  end
-
+#    puts response_for(input)
+#  end 
 
 end
 
-#bob = Bob.new
-#bob
+# bob = Bob.new
+# puts bob.response_for("test")
